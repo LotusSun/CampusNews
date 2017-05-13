e90329fe5d8b3dee66938e67653f4b56c4bf960a
lotus <542783390@qq.com>
2017-05-13 17:02:51 +0800

    Initial commit

--- app/src/main/java/com/lotus/campusnews/MainActivity.java
+++ app/src/main/java/com/lotus/campusnews/MainActivity.java
@@ -1,13 +1,32 @@
 package com.lotus.campusnews;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+
 
 public class MainActivity extends AppCompatActivity {
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initialRegister();
+        setSupportActionBar(toolbar);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    private void initialRegister() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+    }
+
 }
+
+
